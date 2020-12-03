39b3b41c6421d11afbfda54db01e43a8417ea433
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-03 18:08:19 +1300

    Fin handleChange function for login

--- client/components/LandingCard.jsx
+++ client/components/LandingCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
+import LoginCard from './LoginCard'
 
 export default class LandingCard extends React.Component {
   render () {
     return (
     // If you click register Route to RegisterCard
     // If you click login Route to LoginCard
+    <>
       <h1>This is Landing Card Component</h1>
+      <LoginCard />
+    </>
     )
   }
 }
--- client/components/LoginCard.jsx
+++ client/components/LoginCard.jsx
@@ -2,25 +2,41 @@ import React from 'react'
 
 export default class LoginCard extends React.Component {
     state = {
-        username: '',
-        password: ''
-      }
+      username: '',
+      password: ''
+    }
 
     // Handles our inputs to change state
     handleChange = (e) => {
-    this.setState({
+      this.setState({
         [e.target.name]: e.target.value
-    })
+      })
     }
 
-    // Handles our click to fire off our 
+    // Handles our click to fire off our
     handleClick = () => {
-     
+
     }
 
     render () {
       return (
-          
+        <>
+        <h1>Login page</h1>
+        <input
+          className="input"
+          name="username"
+          value={this.state.username}
+          placeholder="username"
+          onChange={this.handleChange}
+        ></input>
+        <input
+          className="input"
+          name="password"
+          value={this.state.password}
+          placeholder="password"
+          onChange={this.handleChange}
+        ></input>
+      </>
       )
     }
-  }
+}
