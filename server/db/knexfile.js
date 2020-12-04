1a25293efbf2d16c90f6bcf38c93bc7d877f1886
Michelle (Shelly) Mutu-Grigg <shellymutugrigg@gmail.com>
2020-12-04 14:56:24 +1300

    update knexfile.js for deployment

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: path.join(__dirname, 'migrations')
     },
     seeds: {
       directory: path.join(__dirname, 'prod/seeds')
