f2e7f4ec3589b07231b743b96ebca97801e74170
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-05 17:47:10 +1300

    Started getBirdById route

--- server/routes/birds.js
+++ server/routes/birds.js
@@ -14,4 +14,40 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/bird/:id', (req, res) => {
+  const id = req.params.id
+  db.getBirdsById(id)
+    .then((bird) => {
+      const info = {
+
+      }
+      res.status(200).json(info)
+      return null
+    })
+    .catch(err => {
+      res.status(500).send('DB ERROR ' + err)
+    })
+})
+
+router.get('/', (req, res) => {
+  db.getBirds()
+    .then((allBirds) => {
+      res.status(200).json(allBirds)
+      return null
+    })
+    .catch(err => {
+      res.status(500).send('DB ERROR ' + err)
+    })
+})
+
+router.get('/', (req, res) => {
+  db.getBirds()
+    .then((allBirds) => {
+      res.status(200).json(allBirds)
+      return null
+    })
+    .catch(err => {
+      res.status(500).send('DB ERROR ' + err)
+    })
+})
 module.exports = router
