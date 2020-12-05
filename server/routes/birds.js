9fa86693c07c26d9dff8eacafa4eb93848e414cc
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-05 16:41:44 +1300

    birds db functions done

---
+++ server/routes/birds.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const { getTokenDecoder } = require('authenticare/server')
+
+const {
+  getBirds,
+  getBirdsById,
+  getColours,
+  getSizes
+} = require('../db/birds')
