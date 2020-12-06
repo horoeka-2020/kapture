0f5a9da02b1677266d2c5e086526768624ed4a3a
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-06 15:45:35 +1300

    removed unused blocks

--- client/components/QuizCard.jsx
+++ client/components/QuizCard.jsx
@@ -14,14 +14,3 @@ export default class LandingCard extends React.Component {
     )
   }
 }
-
-<nav role="navigation">
-  <ul>
-    <li><a href="#">home</a></li>
-    <li><a href="#">about</a></li>
-    <li><a href="#">quiz</a></li>
-    <li><a href="#">gallery</a></li>
-    <li><a href="#">contact us</a></li>
-  </ul>
-</nav>
-{ /* <ImageSlider /> */ }
