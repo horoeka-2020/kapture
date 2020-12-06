a43618382ed93d01c776c5c1300edc42f27eb1e4
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-06 13:53:57 +1300

    remove unused code

--- client/api.js
+++ client/api.js
@@ -41,43 +41,6 @@ export function getSizes (consume = requestor) {
     .catch(errorHandler)
 }
 
-// // Create getObjects function for api on client components side
-// export function getObjects () {
-//   return request
-//     .get(objectUrl)
-//     .then(response => response.body)
-//     .catch(errorHandler('GET', '/'))
-// }
-
-// // Create getObject function for api on client components side
-// export function getObject (objectId) {
-//   return request.get(`/api/v1/objects/${objectId}`)
-//     .then(res => {
-//       return res.body
-//     })
-//     .catch(errorHandler('GET', '/:id'))
-// }
-
-// // Create updateObject function for api on client components side
-// export function updateObject (object) {
-//   return request.patch(`/api/v1/objects/${object.id}`)
-//     .send(object)
-//     .then(res => {
-//       return res.body
-//     })
-//     .catch(errorHandler('PATCH', `/objects/${object.id}`))
-// }
-
-// // Create deleteObject function for api on client components side
-// export function deleteObject (objectId) {
-//   console.log("api.js deleteObject:", objectId)
-//   return request.del(`/api/v1/objects/${objectId}`)
-//     .then(res => {
-//       return res.body
-//     })
-//     .catch(errorHandler('DELETE', '/api/v1/objects/:id'))
-// }
-
 // Handle errors in application
 function errorHandler (method, route) {
   return (err) => {
