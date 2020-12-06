2c7ac6b001eb0e5dce4f846e3a7b2a124f234bb7
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-12-06 13:42:08 +1300

    clientside-api

--- client/api.js
+++ client/api.js
@@ -1,20 +1,9 @@
 // Create client portion of api functions
-// import request from 'superagent'
 import requestor from './consume'
 import { showError } from './actions/error'
 import { dispatch } from './store'
 
-// Create createUser function for api on client components side
-// export function createUser (user, consume = requestor) {
-//   return consume('/users', 'post', user)
-//     .then(res => {
-//       return res.body
-//     })
-//     .catch(errorHandler('POST', '/'))
-// }
-// import request from 'superagent'
-
-// const userUrl = 'http://localhost:3000/api/v1/users'
+const apiUrl = '/api/v1/birds'
 
 // Create createUser function for api on client components side
 export function createUser (user, consume = requestor) {
@@ -23,9 +12,33 @@ export function createUser (user, consume = requestor) {
       return res.body
     })
     .catch((error) => {
-    //   console.log(error.message)
       dispatch(showError(error.message))
     })
+}
+
+// Bird APIs
+export function getAllBirds (consume = requestor) {
+  return consume(apiUrl + '/bird/', 'get')
+    .then((res) => res.body)
+    .catch(errorHandler)
+}
+
+export function getBirdByID (id, consume = requestor) {
+  return consume(apiUrl + '/birds/' + id, 'get')
+    .then((res) => res.body)
+    .catch(errorHandler)
+}
+
+export function getColours (consume = requestor) {
+  return consume(apiUrl + '/colour', 'get')
+    .then((res) => res.body)
+    .catch(errorHandler)
+}
+
+export function getSizes (consume = requestor) {
+  return consume(apiUrl + '/size', 'get')
+    .then((res) => res.body)
+    .catch(errorHandler)
 }
 
 // // Create getObjects function for api on client components side
@@ -66,12 +79,12 @@ export function createUser (user, consume = requestor) {
 // }
 
 // Handle errors in application
-// function errorHandler (method, route) {
-//   return (err) => {
-//     if (err.message === 'Not Found') {
-//       throw Error(`Error: API route for ${method} ${route} ${err.message} missing`)
-//     } else {
-//       throw Error(`${err.message} on ${method} ${route}`)
-//     }
-//   }
-// }
+function errorHandler (method, route) {
+  return (err) => {
+    if (err.message === 'Not Found') {
+      throw Error(`Error: API route for ${method} ${route} ${err.message} missing`)
+    } else {
+      throw Error(`${err.message} on ${method} ${route}`)
+    }
+  }
+}
--- client/api.test.js
+++ client/api.test.js
@@ -1,4 +1,10 @@
-import { createUser } from './api'
+import {
+  createUser,
+  getAllBirds,
+  getBirdByID,
+  getColours,
+  getSizes
+} from './api'
 
 test('createUser returns a valid user', () => {
   const consume = () => Promise.resolve({
@@ -15,6 +21,117 @@ test('createUser returns a valid user', () => {
   return createUser(user, consume)
     .then((body) => {
       expect(body.email).toBe('gmail@email.com')
+      return null
+    })
+})
+
+test('getAllBirds returns all birds', () => {
+  const consume = () => Promise.resolve({
+    body: [{
+      id: 1,
+      name: 'Parson Bird',
+      ignoa: 'Tūī',
+      description: 'Tūī are unique to New Zealand and belong to the honeyeater family',
+      image: '/images/tui.jpg',
+      found_in: 'North, South and Stewart Islands, and their offshore islands',
+      sound: '',
+      conservation_status: 'Not Threatened',
+      threats: 'Predation, habitat loss',
+      colour_id: 1,
+      size_id: 2
+    },
+    {
+      id: 2,
+      name: 'Bellbird',
+      ignoa: 'Korimako',
+      description: 'The melodious bellbird is still widespread but mammalian predators keep their numbers low.',
+      image: '/images/bellbird.jpg',
+      found_in: 'North, South, Stewart and Auckland Islands and many offshore islands',
+      sound: '',
+      conservation_status: 'Not Threatened',
+      threats: 'Predation',
+      colour_id: 4,
+      size_id: 1
+    }]
+  })
+  return getAllBirds(consume)
+    .then((body) => {
+      expect(body[0].description).toBe('Tūī are unique to New Zealand and belong to the honeyeater family')
+      expect(body[1].name).toBe('Bellbird')
+      expect(body).toHaveLength(2)
+      return null
+    })
+})
+
+test('getBirdByID returns a single bird', () => {
+  const consume = () => Promise.resolve({
+    body: {
+      id: 1,
+      name: 'Parson Bird',
+      ignoa: 'Tūī',
+      description: 'Tūī are unique to New Zealand and belong to the honeyeater family',
+      image: '/images/tui.jpg',
+      found_in: 'North, South and Stewart Islands, and their offshore islands',
+      sound: '',
+      conservation_status: 'Not Threatened',
+      threats: 'Predation, habitat loss',
+      colour_id: 1,
+      size_id: 2
+    }
+  })
+
+  const id = consume.id
+
+  return getBirdByID(id, consume)
+    .then((bird) => {
+      expect(bird.id).toBe(1)
+      return null
+    })
+})
+
+test('getColours returns all colours', () => {
+  const consume = () => Promise.resolve({
+    body: {
+      id: '1',
+      name: 'white',
+      hex: '#FFFFFF'
+    }
+
+  })
+  return getColours(consume)
+    .then((colour) => {
+      expect(colour.name).toBe('white')
+      return null
+    })
+})
+
+test('getSizes returns all sizes', () => {
+  const consume = () => Promise.resolve({
+    body: [
+      {
+        id: 1,
+        name: 'tiny',
+        height: 5,
+        width: 5
+      },
+      {
+        id: 2,
+        name: 'small',
+        height: 10,
+        width: 10
+      },
+      {
+        id: 3,
+        name: 'medium',
+        height: 30,
+        width: 30
+      }]
+  })
+
+  return getSizes(consume)
+    .then((size) => {
+      expect(size[2].id).toBe(3)
+      expect(size).toHaveLength(3)
       return null
     })
 })
