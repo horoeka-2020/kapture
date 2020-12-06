63cf965e59d72864913557e6eef633429fc15e64
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-12-07 10:30:37 +1300

    complete authenticare implementation for login & register

--- client/components/loginCardHelper.js
+++ client/components/loginCardHelper.js
@@ -7,15 +7,12 @@ import { showError } from '../actions/error'
 export function signInUser (user, navigateTo) {
   const { username, password } = user
   dispatch(setWaiting())
-  console.log('loginCardHelper.js > signInUser > BEFORE signIn:', user)
   return signIn({ username, password }, { baseUrl: '/api/v1' })
     .then(() => {
-      console.log('loginCardHelper.js > signInUser > BEFORE isAuthenticated:', user)
       if (isAuthenticated()) {
-        console.log('loginCardHelper.js > signInUser > AFTER isAuthenticated:', user)
         const { username, isAdmin, gardenId } = getDecodedToken()
         dispatch(setUser({ username, isAdmin, gardenId }))
-        navigateTo('/garden')
+        navigateTo('/home')
       } else {
         throw new Error('Not authenticated')
       }
--- server/db/users.js
+++ server/db/users.js
@@ -16,18 +16,15 @@ function getUsers (db = connection) {
 
 // Create createUser function for api on server/database side
 function createUser (user, db = connection) {
-  console.log('db/users.js > createUser:', user)
   return userExists(user.username, db)
     .then(exists => {
       if (exists) {
         throw new Error('Bird already in flock')
       }
-      console.log('db/users.js > createUser > user does not exist:', user)
       return false
     })
     .then(() => generateHash(user.password))
     .then(passwordHash => {
-      console.log('db/users.js > createUser > insert user:', user)
       return db('users').insert({
         username: user.username,
         hash: passwordHash,
@@ -38,7 +35,6 @@ function createUser (user, db = connection) {
 
 // Create userExists function to see if user aleady exists
 function userExists (username, db = connection) {
-  console.log('db/users.js > userExists:', username)
   return db('users')
     .count('id as n')
     .where('username', username)
@@ -48,22 +44,11 @@ function userExists (username, db = connection) {
 }
 
 function getUserByName (username, db = connection) {
-  console.log('db/users.js > getUserByName:', username)
   return db('users')
     .select('username', 'is_admin as isAdmin', 'id', 'hash')
     .where('username', username)
     .first()
+    .then((user) => {
+      return user
+    })
 }
-
-// Test to be added back into userExists.test.js when authenticare working
-// txest('createUser creates a new user', () => {
-//     const user = {
-//       email: 'abc@gmail.com',
-//       password: '12345'
-//     }
-//     return users.createUser(user, testDb)
-//       .then((users) => {
-//         expect(user.email).toBe('abc@gmail.com')
-//         return null
-//       })
-//   })
