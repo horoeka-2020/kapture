87e6aa73e83591f7d768e16d1f9c2c913fc1469d
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-12-06 17:05:24 +1300

    style QuizResult.jsx component

--- client/components/QuizResult.jsx
+++ client/components/QuizResult.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getSpecificBird } from './quizResultHelper'
 
-class LandingCard extends React.Component {
+class QuizResult extends React.Component {
     state = {
       birdName: '',
       birdIgnoa: '',
@@ -20,16 +20,17 @@ class LandingCard extends React.Component {
     }
 
     componentDidMount () {
-    // if (isAuthenticated()) {
-    //   const { username, isAdmin, gardenId } = getDecodedToken()
-    //   this.props.dispatch(setUser({ username, isAdmin, gardenId }))
-      return getSpecificBird(1)
+      return getSpecificBird(3)
         .then(bird => {
           this.setState(bird)
           return null
         })
     }
-    // }
+
+    // Handles click to confirm siting and navigate to badges
+    handleClick = (e) => {
+      e.preventDefault()
+    }
 
     render () {
       const {
@@ -38,45 +39,43 @@ class LandingCard extends React.Component {
         birdDesc,
         birdImage,
         birdFoundIn,
-        birdSound,
+        // birdSound,
         birdCons,
         birdThreat,
         birdColourName,
-        birdColourHex,
-        birdSize,
-        birdHeight,
-        birdWidth,
-        birdSizeId
+        // birdColourHex,
+        // birdHeight,
+        // birdWidth,
+        // birdSizeId,
+        birdSize
+
       } = this.state
       return (
         <>
-          {/* <nav role="navigation">
-          <ul>
-            <li><a href="#">home</a></li>
-            <li><a href="#">about</a></li>
-            <li><a href="#">quiz</a></li>
-            <li><a href="#">gallery</a></li>
-            <li><a href="#">contact us</a></li>
-          </ul>
-        </nav> */}
           <div className='wraper'></div>
           <div className='box'>
             <h1 className='cardTitle'>Congratulations! You probably saw a:</h1>
-            <p className='cardText'>Name: {birdName}</p>
-            <p className='cardText'>{birdIgnoa}</p>
-            <p className='cardText'>{birdDesc}</p>
-            <p className='cardText'>{birdImage}</p>
-            <p className='cardText'>{birdFoundIn}</p>
-            <p className='cardText'>{birdSound}</p>
-            <p className='cardText'>{birdCons}</p>
-            <p className='cardText'>{birdThreat}</p>
-            <p className='cardText'>{birdColourName}</p>
-            <p className='cardText'>{birdColourHex}</p>
-            <p className='cardText'>{birdSize}</p>
-            <p className='cardText'>{birdHeight}</p>
-            <p className='cardText'>{birdWidth}</p>
-            <p className='cardText'>{birdSizeId}</p>
-            <div className="colorPick">
+            <div className='flex-container'>
+              <div className='birdText'>
+                <img className='birdImg' src={birdImage} ></img>
+                <br></br>
+                <input
+                  className="btn-submit-bird"
+                  type="submit"
+                  name=""
+                  value="Confirm"
+                  onClick={this.handleClick}/>
+              </div>
+              <div>
+                <h3 className='birdText'>Māori Name: {birdIgnoa}</h3>
+                <h3 className='birdText'>Name: {birdName}</h3>
+                <h3 className='birdText'>Description: {birdDesc}</h3>
+                <h3 className='birdText'>Found In: {birdFoundIn}</h3>
+                <h3 className='birdText'>Conservation Status: {birdCons}</h3>
+                <h3 className='birdText'>Threats: {birdThreat}</h3>
+                <h3 className='birdText'>Main Colour: {birdColourName}</h3>
+                <h3 className='birdText'>Size: {birdSize}</h3>
+              </div>
             </div>
           </div>
         </>
@@ -84,4 +83,4 @@ class LandingCard extends React.Component {
     }
 }
 
-export default LandingCard
+export default QuizResult
