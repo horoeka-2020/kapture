1523762219acfb8095ac5d69a13f0f1caac07deb
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-12-07 10:53:22 +1300

    Create handleClick function to link to the QuizResult component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,16 +6,18 @@ import { connect } from 'react-redux'
 import LandingCard from './LandingCard'
 import Home from './Home'
 import QuizCard from './QuizCard'
+import QuizResult from './QuizResult'
 
 class App extends React.Component {
   render () {
     return (
       <div className='app'>
         <img className="logo" src="../../images/apture.png" alt='logo' width='25%'></img>
-        <Redirect from="/" to="/welcome"/>
+        {/* <Redirect from="/" to="/welcome"/> */}
         <Route path="/welcome" component={LandingCard}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/quiz" component={QuizCard}/>
+        <Route exact path="/quiz/result" component={QuizResult}/>
         <p className='footer'>&#169; Kapture by Slick</p>
       </div>
     )
