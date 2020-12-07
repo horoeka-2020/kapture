dc2e10fe421a984829278651452a59642018871b
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-12-07 15:28:57 +1300

    Create Main Navigation for Home page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,15 +11,15 @@ import QuizResult from './QuizResult'
 class App extends React.Component {
   render () {
     return (
-      <div className='app'>
+      <main>
         <img className="logo" src="../../images/apture.png" alt='logo' width='25%'></img>
-        <Redirect from="/" to="/welcome"/>
+        {/* <Redirect from="/" to="/welcome"/> */}
         <Route path="/welcome" component={LandingCard}/>
         <Route exact path="/home" component={Home}/>
         <Route exact path="/quiz" component={QuizCard}/>
         <Route exact path="/quiz/result" component={QuizResult}/>
         <p className='footer'>&#169; Kapture by Slick</p>
-      </div>
+      </main>
     )
   }
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 
 import ImageSlider from './ImageSlider'
+import Header from './Header'
 
 export default class Home extends React.Component {
   render () {
     return (
       <>
-        <nav role="navigation">
-          <ul>
-            <li><a href="#">home</a></li>
-            <li><a href="#">about</a></li>
-            <li><a href="#">quiz</a></li>
-            <li><a href="#">gallery</a></li>
-            <li><a href="#">contact us</a></li>
-          </ul>
-        </nav>
+        <Header />
         <ImageSlider />
       </>
     )
