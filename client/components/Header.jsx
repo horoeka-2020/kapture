05c6e8dcafcad844a3fd24cfc29977475ccda3df
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-12-08 10:02:27 +1300

    Change the Gallery nav to Sightings nav

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -11,8 +11,8 @@ export default class Home extends React.Component {
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/quiz">Quiz</Link></li>
-            <li><Link to="/gallery">Gallery</Link></li>
-            <li><Link to="/contact">Contact Us</Link></li>
+            <li><Link to="/sightings">Sightings</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
         <hr className='nav-underline'/>
