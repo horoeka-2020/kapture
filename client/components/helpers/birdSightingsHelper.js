cf6d00e42b55763ced0c601a8f3fe1c0c3b5efef
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-12-08 16:44:11 +1300

    update birdSightings functionality

--- client/components/helpers/birdSightingsHelper.js
+++ client/components/helpers/birdSightingsHelper.js
@@ -1,16 +1,3 @@
-// import { dispatch } from '../../store' // , getState
-// import { setWaiting, clearWaiting } from '../../actions/waiting'
-// import { showError } from '../../actions/error'
-// import { getBirdSightings } from '../../api/birdSightings'
-
 export function getAllBirdSightings (userId) {
-  // dispatch(setWaiting())
-  // return getBirdSightings(userId)
-  //   .then(sightings => {
-  //     dispatch(clearWaiting())
-  //     return sightings
-  //   })
-  //   .catch((error) => {
-  //     dispatch(showError(error.message))
-  //   })
+
 }
