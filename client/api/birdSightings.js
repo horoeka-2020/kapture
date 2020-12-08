f7ea0349837f4e66009d55e94899644c59d185b2
rodrigo-diaz <rodrigohdiaz@gmail.com>
2020-12-08 17:24:38 +1300

    finish implementation of bird sightings

---
+++ client/api/birdSightings.js
@@ -0,0 +1,27 @@
+// Create client portion of api functions
+import requestor from './consume'
+
+// Bird Sightings APIs
+export function getBirdSightings (userId, consume = requestor) {
+  return consume(`/sightings/${encodeURIComponent(userId)}`, 'get', {})
+    .then((res) => res.body)
+    .catch(errorHandler)
+}
+
+// Bird Sightings APIs
+export function addBirdSighting (username, latitude, longitude, birdName, consume = requestor) {
+  return consume(`/sightings/${encodeURIComponent(username)}/${encodeURIComponent(latitude)}/${encodeURIComponent(longitude)}/${encodeURIComponent(birdName)}`, 'post', {})
+    .then((res) => res.body)
+    .catch(errorHandler)
+}
+
+// Handle errors in application
+function errorHandler (method, route) {
+  return (err) => {
+    if (err.message === 'Not Found') {
+      throw Error(`Error: API route for ${method} ${route} ${err.message} missing`)
+    } else {
+      throw Error(`${err.message} on ${method} ${route}`)
+    }
+  }
+}
