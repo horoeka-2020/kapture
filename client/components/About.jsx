b12251d4b52bd10535ec4982c1201b732313ff6e
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-12-09 21:31:50 +1300

    Adjust animation for landing

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -15,20 +15,23 @@ export default class About extends React.Component {
               <img className="" src='/images/build/team.jpg' alt='Team' /></span></p>
           </div>
           <div className='about-container'>
-            <h1><span>Shelly (Michelle) Mutu-Grigg | Tui</span></h1>
-            <p className='about-shelly'><span><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+            <h1><span>Shelly (Michelle) Mutu-Grigg | Kākā</span></h1>
+            <p className='about-shelly'><span><strong>Kākā</strong> Make lots of noise....
+              <br></br>
+
+            </span></p>
           </div>
           <div className='about-container'>
-            <h1><span>David Steel | Tui</span></h1>
-            <p className='about-dave'><span><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+            <h1><span>David Steel | Rifleman</span></h1>
+            <p className='about-dave'><span><strong>Rifleman</strong> Strong and persistent</span></p>
           </div>
           <div className='about-container'>
             <h1><span>Rodrigo Diaz | Kea</span></h1>
-            <p className='about-rod'><span><strong>Kea</strong> The endangered kea is the world&aposs only alpine parrot, and one of the most intelligent birds. The kea was crowned Bird of the Year in 2017.</span></p>
+            <p className='about-rod'><span><strong>Kea</strong> The endangered kea is the world&apos;s only alpine parrot, and one of the most intelligent birds. The kea was crowned Bird of the Year in 2017.</span></p>
           </div>
           <div className='about-container'>
             <h1><span>Kittisan P. | Chatham Islands black robin</span></h1>
-            <p className='about-kitti'><span><strong>Chatham Islands black robin</strong> All of today&aposs black robins are descended from the last breeding female, Old Blue. She was one of the longest-lived robins known, reaching 14 years old.</span></p>
+            <p className='about-kitti'><span><strong>Chatham Islands black robin</strong> All of today&apos;s black robins are descended from the last breeding female, Old Blue. She was one of the longest-lived robins known, reaching 14 years old.</span></p>
           </div>
         </div>
         <Footer />
