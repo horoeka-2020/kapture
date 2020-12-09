8143bcafd436fa8323c5ba9e7851084e03ec07a0
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-12-09 19:50:25 +1300

    update bird sightings to userId instead of being hard coded

--- client/components/BirdSightings.jsx
+++ client/components/BirdSightings.jsx
@@ -1,3 +1,4 @@
+import { getDecodedToken } from 'authenticare/client/auth'
 import React from 'react'
 import { connect } from 'react-redux'
 
@@ -5,19 +6,38 @@ import Map from './Map'
 import Header from './Header'
 import Footer from './Footer'
 import { getAllBirdSightings } from './helpers/birdSightingsHelper'
+import { setUserLocation } from '../actions/user'
 
 class BirdSightings extends React.Component {
-    state = {
-      sightings: []
+  constructor (props) {
+    super(props)
+    this.state = {
+      id: '',
+      sightings: [],
+      userCoordinates: []
+    }
+  }
+
+  componentDidMount () {
+    const setLocation = (location) => {
+      this.props.dispatch(setUserLocation(location))
+      this.setState({ userCoordinates: [{ latitude: location.latitude, longitude: location.longitude }] })
+    }
+    const id = getDecodedToken().id
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords
+        setLocation({ latitude, longitude })
+      })
     }
 
-    componentDidMount () {
-      return getAllBirdSightings(2)
-        .then(sightings => {
-          this.setState({ sightings: sightings })
-          return sightings
-        })
-    }
+    return getAllBirdSightings(id)
+      .then(sightings => {
+        this.setState({ sightings: sightings })
+        console.log(this.state)
+        return sightings
+      })
+  }
 
     // Handles click to confirm siting and navigate to badges
     handleClick = (e) => {
@@ -26,7 +46,8 @@ class BirdSightings extends React.Component {
 
     render () {
       const {
-        sightings
+        sightings,
+        userCoordinates
       } = this.state
       return (
         <>
@@ -35,7 +56,7 @@ class BirdSightings extends React.Component {
           <div className='box-sightings'>
             <h2 className='cardTitle'>Bird Sightings</h2>
             <div className='about-contrainer'>
-              <Map sightings={sightings} handleClick={this.handleClick} />
+              <Map sightings={sightings} userCoordinates={userCoordinates} handleClick={this.handleClick} />
             </div>
           </div>
           <Footer />
@@ -44,4 +65,10 @@ class BirdSightings extends React.Component {
     }
 }
 
-export default connect()(BirdSightings)
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(BirdSightings)
