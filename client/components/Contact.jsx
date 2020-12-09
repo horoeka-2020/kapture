4e394745723d43fba51aa2711c6f0623beb426a8
Kittisan P <70613193+Kittisan-P@users.noreply.github.com>
2020-12-09 18:46:19 +1300

    About page

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -8,56 +8,58 @@ export default class About extends React.Component {
       <>
         <Header />
         <div className='wraper'></div>
-        <div className='box'>
-          {/* <div className='about-contrainer'> */}
-          <div className="inputBox">
-            <h1><span>Contact Us</span></h1>
-            <form>
-              <input
-                className="input"
-                id="name"
-                type="text"
-                autoComplete="name"
-                name="name"
-                // value={this.state.name}
-                placeholder="Please enter your name"
-                onChange={this.handleChange}
-              ></input>
+        <div className='box-wrapper'>
+          <div className='box'>
+            {/* <div className='about-contrainer'> */}
+            <div className="inputBox">
+              <h1><span>Contact Us</span></h1>
+              <form>
+                <input
+                  className="input"
+                  id="name"
+                  type="text"
+                  autoComplete="name"
+                  name="name"
+                  // value={this.state.name}
+                  placeholder="Please enter your name"
+                  onChange={this.handleChange}
+                ></input>
 
-              <input
-                className="input"
-                id="email"
-                type="text"
-                autoComplete="email"
-                name="email"
-                // value={this.state.email}
-                placeholder="Please enter your email address"
-                onChange={this.handleChange}
-              ></input>
+                <input
+                  className="input"
+                  id="email"
+                  type="text"
+                  autoComplete="email"
+                  name="email"
+                  // value={this.state.email}
+                  placeholder="Please enter your email address"
+                  onChange={this.handleChange}
+                ></input>
 
-              <textarea
-                className="input"
-                id="message"
-                type="text"
-                autoComplete="message"
-                name="message"
-                // value={this.state.message}
-                placeholder="Please enter your message for the flock"
-                onChange={this.handleChange}
-              ></textarea>
-            </form>
-            <input
-              className="btn-submit"
-              type="submit"
-              data-testid="submit-button"
-              name="login-submit"
-              value="Submit"
-              onClick={this.handleClick}/>
-            {/* </section> */}
+                <textarea
+                //   className="input"
+                  maxLength="500"
+                  id="message"
+                  type="text"
+                  autoComplete="message"
+                  name="message"
+                  // value={this.state.message}
+                  placeholder="Please enter your message for the flock"
+                  onChange={this.handleChange}
+                ></textarea>
+              </form>
+              <input
+                className="btn-submit"
+                type="submit"
+                data-testid="submit-button"
+                name="login-submit"
+                value="Submit"
+                onClick={this.handleClick}/>
+              {/* </section> */}
+            </div>
           </div>
         </div>
         <Footer />
-
       </>
     )
   }
