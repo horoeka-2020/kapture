9c336e99434981b915ccd12c8ce23bf046d4614d
shellymutu-grigg <shellymutugrigg@gmail.com>
2020-12-17 07:56:05 +1300

    add in animation for load of profile component

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -50,38 +50,40 @@ class Profile extends React.Component {
     return (
       <>
         <div className='wraper'></div>
-        <Header />
-        <h1 className='cardTitleProfile'>Welcome {this.state.username}!</h1>
-        <div className="flex-wrapper">
+        <div className='profile-wrapper'>
+          <Header />
+          <h1 className='cardTitleProfile'>Welcome {this.state.username}!</h1>
+          <div className="flex-wrapper">
 
-          {/* Bird Sightings Card */}
-          <div className='user-card col'>
-            <h1 className='userCardTitle'>Birds you have found!</h1>
-            <p className='userText'>You have had <span className="proud">{this.state.sightings.length}</span> bird sightings!</p>
-            {this.state.sightings.map((bird) => (
-              <div key={bird.birdId}>
-                <span className="mapText">{bird.birdName}</span>
-                <span className="mapText">{bird.birdIgnoa}</span>
-                <span className="mapText">Kaptured on the {bird.birdDate}</span>
-                <span className="mapText">Found in {bird.birdFoundIn}</span>
-              </div>
-            ))}
+            {/* Bird Sightings Card */}
+            <div className='user-card col'>
+              <h1 className='userCardTitle'>Birds you have found!</h1>
+              <p className='userText'>You have had <span className="proud">{this.state.sightings.length}</span> bird sightings!</p>
+              {this.state.sightings.map((bird) => (
+                <div key={bird.birdId}>
+                  <span className="mapText">{bird.birdName}</span>
+                  <span className="mapText">{bird.birdIgnoa}</span>
+                  <span className="mapText">Kaptured on the {bird.birdDate}</span>
+                  <span className="mapText">Found in {bird.birdFoundIn}</span>
+                </div>
+              ))}
+            </div>
+
+            {/* Badges Card */}
+            <div className='user-card col'>
+              <h1 className='userCardTitle'>Badges you have collected!</h1>
+              <p className='userText'>You have earnt <span className="proud">{this.state.badges.length}</span> badges</p>
+              {this.state.badges.map((badge) => (
+                <div className="badge-container" key={badge.id}>
+                  <img className="badge" src={badge.image} alt=""/>
+                  <p className="badge-description">Congratulations you have earned the {badge.name}!</p>
+                </div>
+              ))}
+            </div>
+
           </div>
-
-          {/* Badges Card */}
-          <div className='user-card col'>
-            <h1 className='userCardTitle'>Badges you have collected!</h1>
-            <p className='userText'>You have earnt <span className="proud">{this.state.badges.length}</span> badges</p>
-            {this.state.badges.map((badge) => (
-              <div className="badge-container" key={badge.id}>
-                <img className="badge" src={badge.image} alt=""/>
-                <p className="badge-description">Congratulations you have earned the {badge.name}!</p>
-              </div>
-            ))}
-          </div>
-
+          <Footer />
         </div>
-        <Footer />
       </>
     )
   }
